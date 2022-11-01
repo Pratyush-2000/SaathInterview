ebb8a56dc8da389bf846dce94dd9721fe54fc5ac
CodingChase <pratyushagarwal4@gmail.com>
2022-11-01 22:06:59 +0530

    Rectified Debouncing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect,useCallback } from 'react';
+
 
 function App() {
+
+  const [query,setQuery] = useState("");
+  const [boxCount,setBoxCount] = useState(0);
+  const [boxes,setBoxes] = useState([]);
+
+
+  const  updateBoxes = () => {
+
+
+    let temp = []
+    
+    for(let i=0 ; i<boxCount ;i++){
+      temp.push(i);
+    }
+
+    setBoxes(temp);
+
+
+  }
+
+  const debounce = (func) => {
+
+    let timer;
+
+    return function(...args) {
+
+      const context = this;
+      if(timer) clearTimeout(timer);
+      timer =setTimeout(() => {
+        timer = null
+        func.apply(context,args);
+      }, 500);
+
+    }
+
+  }
+
+  const incrementCount = () => {
+    setBoxCount((old) => old+1);
+  }
+
+
+  useEffect(() =>{
+
+    const debounceUpdateBoxes = debounce(incrementCount);
+
+    window.addEventListener('resize',debounceUpdateBoxes);
+
+    return () => {
+      window.removeEventListener('resize',debounceUpdateBoxes);
+    }; 
+
+  },[])
+
+  useEffect(() =>{
+
+    updateBoxes();
+
+  },[boxCount])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type="number" 
+        onChange={(e) => {
+          setQuery(Number(e.target.value));
+        }}>
+        </input>
+        <button onClick={(e) => setBoxCount((old) => old+query)}>
+          Submit
+        </button>
+      
+
+      <div className="box-container">
+
+        {boxes.map((box,index)=>{
+            return <div key={index} className="box"></div>
+        })}
+
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// 1. Create a textbox and a submit button.,
+//  the textbox specifies the number of boxes to be added on screen. The boxes should appear besides each other, 100px x 100px, grey in color, margin 5px, and should wrap to the next line when more are added.
+// 2. Now boxes should be added whenever screen is resized.
+// 3. addition of elements such that every 3 seconds only one element is added
+// 4. addition of elements such that element is only added when resize stops for 3s
+
+/**
+ * 100px x 100px, grey in color, margin 5px, and should wrap to the next line when more are added
+ */
---
+++ src/Throttle.js
@@ -0,0 +1,8 @@
+
+export const throttle = (fn,limit) {
+
+    flag=true
+
+
+
+}
